fbc1402060413fe0142ad59e5c65fe31820466d4
kapilgothi001 <kapilgothi001@gmail.com>
2025-06-13 00:27:22 +0530

    JSX and react component reset

--- App.js
+++ App.js
@@ -3,39 +3,80 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React Element
-const jsxHeading = (
-  <h1 className="head" tabIndex="3" id="heading">
-    Namste React Kapil
-  </h1>
-);
+// const jsxHeading = (
+//   <h1 className="head" tabIndex="3" id="heading">
+//     Namste React Kapil
+//   </h1>
+// );
 
-console.log(jsxHeading);
+// console.log(jsxHeading);
 
-//React component Name start from capital letter [Title is component]
-const Title = () => (
-  <h1 className="head" tabIndex="3" id="heading">
-    This is Title Tag component
-  </h1>
-);
+// //React component Name start from capital letter [Title is component]
+// const Title = () => (
+//   <h1 className="head" tabIndex="3" id="heading">
+//     This is Title Tag component
+//   </h1>
+// );
 
 //[title is Normal JS varable]
-const title = <h1>This is Title variable of js</h1>;
+// const title = (
+//   <h1>
+//     {/* <Title /> */}
+//     {ele}
+//     This is Title variable of js
+//   </h1>
+// );
+
+// const title2 = (
+//   <div>
+//     {ele}
+//     <h1> This is Title variable of js</h1>
+//   </div>
+// );
+
+// const title = (
+//   <>
+//     {ele}
+//     <h1> This is Title variable of js</h1>
+//   </>
+// );
+
+// React functional component
+// const HeadingComponent = () => (
+//   <div id="container">
+//     <Title />
+
+//     <h4>{Number}</h4>
+
+//     {title}
+
+//     <h1 className="head" tabIndex="3">
+//       This is Heading component
+//     </h1>
+//   </div>
+// );
+
+// const Title = () => (
+//   <h1 className="head" tabIndex="5">
+//     {/* {ele} */}
+//     Namste React using JSX
+//   </h1>
+// );
+
+const ele = <span>Kapil</span>;
 
 const Number = 1000;
 
-// React functional component
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namste React using JSX
+  </h1>
+);
+
 const HeadingComponent = () => (
   <div id="container">
     <Title />
-{/*  */}
-    <h4>{Number}</h4>
-
-    {title}
-
-    <h1 className="head" tabIndex="3">
-      This is Heading component
-    </h1>
-
+    <h1 className="heading">Namste React functional component</h1>
   </div>
 );
 
@@ -43,6 +84,18 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<HeadingComponent />);
 
+// const HeadingComponent = () => (
+//   <div id="container">
+//     <Title />
+//     {/* {Title()} */}
+//     {/* <Title></Title> */}
+//     {/* <h4>{Number}</h4> */}
+//     {/* {title} */}
+//     <h1 className="heading">Namste React functional component</h1>
+//   </div>
+// );
+
+// root.render(title);
 //for javascript
 // const heading1 = document.createElement("h1");
 // heading1.innerHTML = "Hello world by JS";
